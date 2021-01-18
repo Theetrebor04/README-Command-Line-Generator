207da6e573daa5e0f59bb42004a12eaf0fc08d99
Robert Clawson <bobbycharizard@gmail.com>
2021-01-18 15:28:25 -0700

    Added an array of questions for user input

--- potential-enigma-master/Develop/index.js
+++ potential-enigma-master/Develop/index.js
@@ -1,12 +1,56 @@
 // TODO: Include packages needed for this application
 const generateMarkdown = require('./utils/generateMarkdown.js');
-
-
-
+const inquirer = require('inquirer');
+const fs = require('fs');
 
 // TODO: Create an array of questions for user input
-const questions = [];
-
+const questions = [
+    {
+        type:"input",
+        message: "What is your GitHub username?",
+        name: "UserName",
+    },
+    {
+        type:"input",
+        message: "What is your email address?",
+        name: "Email",
+    },
+    {
+        type: "input",
+        message: "What is the title for your project?",
+        name: "Title",
+    },
+    {
+        type: "input",
+        message: "Please give a description of your project.",
+        name: "Description",
+    },
+    {
+        type: "input",
+        message: "What necessary dependencies must be installed to run this app?",
+        name: "Installation",
+    },
+    {
+        type: "input",
+        message: "What is the app used for?",
+        name: "Usage",
+    },
+    {
+        type: "input",
+        message: "What license was used for the README?",
+        name: "Licence",
+    },
+    {
+        type: "input",
+        message: "Please add contributors.",
+        name: "Contributor",
+    },
+    {
+        type: "input",
+        message: "What command do you use to tes this app?",
+        name: "Test",
+    }
+];
 
 
 // TODO: Create a function to write README file
